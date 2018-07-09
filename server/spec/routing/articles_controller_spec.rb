ac9c25b710bb556e157625458a32bd51cbe006da
Watermelon Puppet <xiguawanou@gmail.com>
2018-07-09 22:46:05 +0200

    Fixed the broken unit tests.

--- server/spec/routing/articles_controller_spec.rb
+++ server/spec/routing/articles_controller_spec.rb
@@ -3,7 +3,7 @@ describe ArticlesController, type: 'controller' do
   it{should route(:put, '/api/articles/featured/1').to(action: :update_featured, id: 1, format: :json)}
 
   it{should route(:get, '/api/articles/preview').to(action: :index_preview, format: :json)}
-  it{should route(:get, '/api/articles/1').to(action: :show, id: 1, format: :json)}
+  it{should route(:get, '/api/articles/1/full').to(action: :show_full, id: 1, format: :json)}
   it{should route(:post, '/api/articles').to(action: :create, format: :json)}
   it{should route(:put, '/api/articles/1').to(action: :update, id: 1, format: :json)}
   it{should route(:delete, '/api/articles/1').to(action: :destroy, id: 1, format: :json)}
