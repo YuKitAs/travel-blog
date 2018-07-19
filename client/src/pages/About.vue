f490019c3c8adbd6b684dbc9c12addfe05344769
YuKitAs <moncher.merci@gmail.com>
2018-07-20 01:29:38 +0200

    Modified text in About.

--- client/src/pages/About.vue
+++ client/src/pages/About.vue
@@ -2,34 +2,34 @@
   <div>
     <h1 class="tb-page-title">About</h1>
     <div class="tb-page-content">
-      <p>Welcome to our Blog! We are Watermelon Puppet and YuKitAs. This tiny Blog records our experiences about travelling and programming.</p>
+      <p>Welcome to our blog! We are Watermelon Puppet and YuKitAs. This tiny blog records our travelling and programming experiences.</p>
 
-      <p>We both love travelling, especially hiking. We step into forests, walk over hills, and pass by villages. While we are far away from the cities, we are also far away from the rushing feelings, so that we can calm ourselves down and think. We also enjoy the different life-styles and cultures in a foreign city. Cities in different countries, event different cities in the same country, they all have their unique side. To reveal the uniqueness of different cities brings us a lot of fun. We also love programming, like making a little step forward each time, and finally bring the ideas in our minds into the real world.</p>
+      <p>We love travelling, especially hiking. We trek through the forests, walk over the hills, visit the villages far away from the hustle and bustle of the cities, so we can calm down and think. We also enjoy different lifestyles in foreign cities and exploring their unique charms.</p>
 
-      <p>We think we are lucky, because there is no contradictory between travelling and programming. Two pairs of trekking poles, two backpacks, two laptops, it is everything for us to enjoy the happy weekend. We hike in the day, and write a few lines of codes in the evening. What could be more satisfying than having a day like this?</p>
+      <p>We love programming too. Every day we make small steps forward and finally bring our ideas to the real world.</p>
 
-      <p>Before we have made this Blog, we collect tickets, post cards and piece of papers, and attach them into a notebook. While we are reading the notebook, all the memories seem to be bringing us back to the places that we have visited before. As the notebook becoming thicker, we start to realize that in this way it is hard to have a complete record of the whole journey. Since, for example, we have taken many photos along the way, but there is no room in the notebook for so many photos. Also, we want to have a map, where our travel destinations are marked. Thus, the idea of having a travel blog comes naturally to our minds.</p>
+      <p>We are lucky because we are able to program while travelling. Two pairs of trekking poles, two backpacks, two laptops, that's all for us to enjoy a happy weekend. We hike in the daytime and code in the evening. What could be more satisfying?</p>
 
-      <p>The name and the LOGO of the Blog comes from our pet Bonbon the Hamster. While we are struggling about the name of the Blog, we have considered lemming. But later we realized that why not just use the name of our pet. In the reality, Bonbon cannot travel with us (hard to carry the cage while hiking, even if we can, changing of the environment will stress hamsters). However in our imagination, it is the captain who brings us to our destinations.</p>
+      <p>The name and the logo of our blog came from our pet Bonbon the Djungarian hamster. Bonbon cannot travel with us, so we programed a feeding machine which can be controlled with our mobile phones when we are not at home. In our minds, Bonbon is like the Captain who guides us to our destinations.</p>
 
-      <p>Although the original purpose of this Blog is to record our own experiences. But if you just occasionally pass by, and find it interesting, we will be very happy.</p>
+      <p>If you pass by our blog and find it interesting, we would be very happy.</p>
     </div>
 
     <hr/>
 
     <h1 class="tb-page-title">关于</h1>
     <div class="tb-page-content">
-      <p>欢迎来到我们的博客。我们是西瓜玩偶和YuKitAs，在这个小小的博客站点里记录了我们的一些有关于旅行和编程的经历。</p>
+      <p>欢迎来到我们的博客！我们是西瓜玩偶和YuKitAs。这个小小的博客记录了我们旅行和编程的经历。</p>
 
-      <p>我们俩都喜欢出门旅行，尤其喜欢徒步——穿过一片片森林，翻过一座座小山，经过一个个小村庄。在远离城市的时候，我们也远离了那种浮躁的感觉，从而能够静下心来思考。我们也喜欢在完全陌生的城市里面体验不同的生活方式与文化，不同国家的城市，甚至是一个国家里的不同的城市，如果肯留心观察的话，总会发现它与众不同的一面。去发现这些城市与众不同的一面让我们乐在其中。我们俩也都非常喜欢写程序，喜欢一步步地实现了自己的点子的那种感觉。</p>
+      <p>我们热爱旅行，尤其是徒步。穿过一片片森林，翻过一座座小山，游览一个个远离城市喧嚣的小村庄，我们可以静下心来思考。我们也喜欢在陌生的城市体验不同的生活方式与文化，探索每座城市独特的魅力。</p>
 
-      <p>让我们俩觉得最幸运的事情是，旅行和编程这两件事情并不矛盾。两幅登山杖、两个背包、两台笔记本电脑，这些就足够让我们度过一个快乐的周末了。白天的时候，我们可以尽情地在山中林间漫步。晚上到了城市里，睡觉之前，我们就可以拿出电脑来码几行代码。这样度过一天会让我们觉得非常充实。</p>
+      <p>我们也热爱编程。每天积累一点微小的进步，直到将我们的想法变为现实。</p>
 
-      <p>在我们有这个博客之前，我们会把我们旅途中收集的车票、明信片和一些值得纪念的小纸片贴到一本本子上，并且附上一段简单的游记，有的时候还会画一些简单的画。当我们回头去看本子上记的内容的时候，回忆就会被唤起，让我们仿佛又回到了我们之前去过的地方。但是当这本本子变得越来越厚的时候，我们开始觉得，这样的方式很难完整地记录旅途中的经历。因为我们往往会在途中拍照片，本子上的空间可不够把我们喜欢的照片贴起来。而且我们经常想要做一张地图，上面标出了所有我们去过的地点。这样一来，我们很自然地就想到了旅行博客的这个点子。</p>
+      <p>让我们觉得最幸运的是，我们能够在旅行中编程。两副登山杖、两个登山包、两台笔记本就足够让我们度过一个愉快的周末了。白天，我们可以在山中林间尽情徒步；晚上，我们可以拿出笔记本来写几行代码。还有比这更充实的一天吗？</p>
 
-      <p>博客的名称和Logo来自于我们养的小宠物——小仓鼠Bonbon。在我们思考给网站起什么名字比较好的时候，我们曾经想过旅鼠。不过后来我们想，为什么不用我们自己家的Bonbon当名称呢。现实中，Bonbon不能跟我们一起旅行（徒步的话没法拿笼子，即便是拿了笼子，换环境也会给仓鼠造成心理负担），不过我们的想象中，它就是带着我们到达目的地的船长。</p>
+      <p>这个博客的名称和logo灵感来源于我们的宠物仓鼠Bonbon。因为Bonbon不能跟我们一起旅行，所以我们为他写了一个可以通过手机遥控的自动喂食器。而在我们心中，Bonbon就像引领我们到达旅行目的地的船长一样。</p>
 
-      <p>虽然这个博客的初衷只是为了记录我们自己的经历，不过如果您恰巧路过，并且觉得里面的内容很有趣的话，我们也会为此非常开心的。</p>
+      <p>如果你恰巧路过我们的博客，并且觉得里面的内容有趣的话，我们会感到非常开心的。</p>
     </div>
 
     <hr/>
