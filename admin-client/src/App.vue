378619de0e405aa9cceecedd496dab0625ab4efb
YuKitAs <moncher.merci@gmail.com>
2018-07-22 03:43:23 +0200

    Made layout responsive.

--- admin-client/src/App.vue
+++ admin-client/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
-    <b-nav vertical class="col-md-2 bg-light sidebar">
-      <b-nav-item disabled><font-awesome-icon class="icon" icon="file-image" fixed-width />Articles</b-nav-item>
-      <b-nav-item disabled><font-awesome-icon class="icon" icon="map-marked-alt" fixed-width />Places</b-nav-item>
-      <b-nav-item><font-awesome-icon class="icon" icon="tag" fixed-width />Tags</b-nav-item>
-    </b-nav>
-    <div class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-      <tags/>
+    <div class="row">
+      <b-nav vertical class="d-none d-md-block col-md-3 col-lg-2 pt-1 bg-light sidebar">
+        <b-nav-item disabled><font-awesome-icon class="icon" icon="file-image" fixed-width />Articles</b-nav-item>
+        <b-nav-item disabled><font-awesome-icon class="icon" icon="map-marked-alt" fixed-width />Places</b-nav-item>
+        <b-nav-item><font-awesome-icon class="icon" icon="tag" fixed-width />Tags</b-nav-item>
+      </b-nav>
+      <div class="col-12 col-md-9 ml-sm-auto col-lg-10 pt-3">
+        <tags/>
+      </div>
     </div>
   </div>
 </template>
@@ -22,24 +24,13 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .sidebar {
   position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
-  z-index: 100;
-  padding: 0;
-  box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
-}
-
-.bg-light {
-  background-color: #f8f9fa!important;
-}
-
-.col-md-2 {
-  flex: 0 0 16.666667%;
-  max-width: 16.666667%;
+  box-shadow: inset -2px 0 6px 0 rgba(0.5, 0.5, 0.5, 0.1);
 }
 
 .icon {
