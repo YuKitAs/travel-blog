ea562bf9ac59d0091371be8a4d0d12769fbebe26
Zihan Lin <zihanlin@flaregames.com>
2018-07-25 22:29:36 +0200

    Adjusted the layout of the admin page.

--- admin-client/src/App.vue
+++ admin-client/src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <div id="app">
-    <div class="menu-container" v-if="authenticated">
-      <b-nav to="/login" vertical class="d-none d-md-block col-md-3 col-lg-2 pt-1 bg-light sidebar">
-        <b-nav-item><font-awesome-icon class="icon" icon="file-image" fixed-width />Articles</b-nav-item>
-        <b-nav-item><font-awesome-icon class="icon" icon="map-marked-alt" fixed-width />Places</b-nav-item>
-        <b-nav-item to="/tags"><font-awesome-icon class="icon" icon="tag" fixed-width />Tags</b-nav-item>
-      </b-nav>
+  <div id="app" class="tb-page-container d-flex flex-row">
+    <div class="tb-menu d-none d-md-block col-md-3 col-lg-2 bg-light" v-if="authenticated">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <font-awesome-icon class="tb-menu__nav-item-icon" icon="file-image" fixed-width/>Articles
+        </li>
+        <li class="nav-item">
+          <font-awesome-icon class="tb-menu__nav-item-icon" icon="map-marked-alt" fixed-width/>Places
+        </li>
+        <li class="nav-item">
+          <font-awesome-icon class="tb-menu__nav-item-icon" icon="tag" fixed-width/>Tags
+        </li>
+      </ul>
     </div>
 
-    <div class="content-container col-12 col-md-9 ml-sm-auto col-lg-10 pt-3">
+    <div class="tb-content" :class="authenticated ? ['ml-md-auto', 'col-md-9', 'col-lg-10'] : ['col-12']">
       <router-view @authenticated="setAuthenticated"/>
     </div>
   </div>
@@ -39,15 +45,20 @@ export default {
 </script>
 
 <style scoped>
-.sidebar {
+.tb-menu {
   position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
+  padding-top: 1em;
   box-shadow: inset -2px 0 6px 0 rgba(0.5, 0.5, 0.5, 0.1);
 }
 
-.icon {
+.tb-menu__nav-item-icon {
   margin-right: 10px;
+}
+
+.tb-content {
+  padding-top: 1em;
 }
 </style>
